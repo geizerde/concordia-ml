ec382ddf35867ff471b676bdc1a1e0cd2d559cb2
KidawR <vadimser2002@mail.ru>
2024-12-01 15:47:18 +0300

    Модель для оценки сообщейний
    Оценка сообщений на аглийском по 5 параметрам

--- ml.py
+++ ml.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 # Загрузка данных
-df = pd.read_csv("/dataset.csv")
+df = pd.read_csv("dataset.csv")
 df.drop(columns=['sex'], inplace=True)
 df = df.dropna()
 # Преобразование категориальных данных с помощью one-hot encoding
@@ -17,10 +17,10 @@ df[['age', 'height']] = scaler.fit_transform(df[['age', 'height']])
 from sklearn.neighbors import NearestNeighbors
 
 # Применение KNN для нахождения ближайших соседей
-knn = NearestNeighbors(n_neighbors=100, metric='cosine')
+count_neighbors = 100
+knn = NearestNeighbors(n_neighbors=count_neighbors, metric='cosine')
 knn.fit(df)
 
-# Для пользователя с индексом 0, находим 5 ближайших соседей
 distances, indices = knn.kneighbors(df.iloc[0].values.reshape(1, -1))
 
 print("Похожие пользователи для пользователя 0:", indices[0][1:])  # Исключаем самого себя
@@ -28,7 +28,8 @@ d_min = np.min(distances)
 d_max = np.max(distances)
 # Нормализуем расстояния, вычитаем из 1 для получения схожести от 0 до 1 и округляем до одного знака после запятой
 normalized_distances = np.round(1 - (distances - d_min) / (d_max - d_min), 1)
-
+"""
 print("Расстояния до ближайших соседей:", normalized_distances)  # Вывод расстояний до ближайших соседей
 for i in indices[0]:
     print(df.iloc[i])  # Вывод данных для каждого из ближайших соседей
+"""
